66624db9109d72726895af775b5bb12171636a1c
Kinga Tokarzewska <kinga@Kingas-MacBook-Air.local>
2017-07-06 21:27:26 +0200

    Correct

--- components/Contact.js
+++ components/Contact.js
@@ -6,11 +6,7 @@ var Contact = React.createClass({
 	render: function() {
 		return (
 			<div className={'contactItem'}>
-<<<<<<< HEAD
 				<img className={'contactImage'} src={'https://avatarfiles.alphacoders.com/495/49573.jpg'} />
-=======
-				<img className={'contactItem'} src={'https://avatarfiles.alphacoders.com/495/49573.jpg'} />
->>>>>>> origin/master
 				<p className={'contactLabel'}>
 					Imię: {this.props.item.firstName}
 				</p>
